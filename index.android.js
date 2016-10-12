0eaabc3198971ed6078a2a14b315ded0c5bd1460
Ranjith Nagella <nagella.ranjith@gmail.com>
2016-10-11 23:36:49 -0700

    rnagella: Updates to package dependency ..

--- index.android.js
+++ index.android.js
@@ -4,7 +4,9 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import {
   AppRegistry,
   StyleSheet,
@@ -14,19 +16,24 @@ import {
 
 class DQRNQUIZ extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+    return ( < View style = {
+        styles.container
+      } >
+      < Text style = {
+        styles.welcome
+      } >
+      Welcome to React Native!
+      < /Text> < Text style = {
+        styles.instructions
+      } >
+      To get started, edit index.android.js < /Text> < Text style = {
+        styles.instructions
+      } >
+      Double tap R on your keyboard to reload, {
+        '\n'
+      }
+      Shake or press menu button
+      for dev menu < /Text> < /View>
     );
   }
 }
--- index.ios.js
+++ index.ios.js
@@ -1,52 +1,86 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+// @flow
+import React, {
+  Component
+} from 'react';
+/* Components */
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
+  Image,
+  TouchableHighlight,
 } from 'react-native';
+/* Navigation */
+import {
+  createRouter,
+  NavigationProvider,
+  StackNavigation,
+  TabNavigation,
+  TabNavigationItem,
+} from '@exponent/ex-navigation';
+/* Icons */
+import Icon from 'react-native-vector-icons/FontAwesome';
+// import Entypo from 'react-native-vector-icons/Entypo';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import Router from './navigation/AppRouter';
+
+function getColor(isSelected) {
+  return isSelected ? '#0084FF' : 'red';
+}
 
 class DQRNQUIZ extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+      return (
+        <NavigationProvider router={Router}>
+          <TabNavigation id = "tab-navigation"
+            navigationUID = "tab-navigation"
+            initialTab = "first">
+              <TabNavigationItem id="first" selectedStyle={styles.selectedTab}
+                renderIcon={(isSelected) => <Ionicons name="ios-boat-outline" size={28} color={getColor(isSelected)} /> }
+              >
+                <StackNavigation id="Screen1Screen" initialRoute={Router.getRoute('screen1')}/>
+              </TabNavigationItem>
+              <TabNavigationItem id="second" 
+                renderIcon={(isSelected) => <Ionicons name="ios-list" size={28} color={getColor(isSelected)}/>} 
+                selectedStyle = {styles.selectedTab} >
+                <StackNavigation id="Screen2Screen" initialRoute = {Router.getRoute('screen2')}/>
+              </TabNavigationItem>
+              <TabNavigationItem
+                id="third"
+                selectedStyle={styles.selectedTab}
+                renderIcon={(isSelected) => <Ionicons name="ios-add" size={28} color={getColor(isSelected)}/>} >
+                  <StackNavigation id="Screen3Screen" initialRoute={Router.getRoute('screen3')}/>
+              </TabNavigationItem>
+              <TabNavigationItem 
+                id="fourth" 
+                selectedStyle={styles.selectedTab} 
+                renderIcon={(isSelected) => <Ionicons name="ios-chatboxes-outline" size={28} 
+                color={getColor(isSelected)}/>} >
+                <StackNavigation id = "Screen4Screen"
+                  initialRoute = {Router.getRoute('screen4')}/>
+              </TabNavigationItem>
+              <TabNavigationItem 
+                id="fifth" 
+                renderIcon={(isSelected) => <Ionicons name="ios-person-outline" size={28} color={getColor(isSelected)} /> } 
+                selectedStyle={styles.selectedTab} >
+                  <StackNavigation id = "Screen5Screen" initialRoute = {Router.getRoute('screen5')} />
+              </TabNavigationItem>
+          </TabNavigation>
+        </NavigationProvider>
+      );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  selectedTab: {
+    borderWidth: 2,
+    borderRadius: 25,
+    height: 50,
+    width: 50,
+    borderColor: '#0084FF'
   },
 });
 
