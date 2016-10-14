3f095bd971791e72f87502fbeed59a92d17c3be0
Ranjith Nagella <nagella.ranjith@gmail.com>
2016-10-13 22:14:28 -0700

    rnagella: Added comments to NavBar scene

--- screens/NavigatorScene.js
+++ screens/NavigatorScene.js
@@ -30,11 +30,10 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 // import Entypo from 'react-native-vector-icons/Entypo';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
+// Highlight tab based on the user selection
 function getColor(isSelected) {
   return isSelected ? '#0084FF' : '#000';
 }
-
-// StatusBar.barStyle = 'default';
 
 export default class NavigatorScene extends Component {
   render() {
@@ -42,23 +41,28 @@ export default class NavigatorScene extends Component {
           <NavigationProvider router={Router}>
             <TabNavigation id = "tab-navigation"
               navigationUID = "tab-navigation"
+              // Set initial Tab to Add/Follow scene
               initialTab = "add">
+              {/* Menu Tab */}
                 <TabNavigationItem id="menu" selectedStyle={styles.selectedTab}
                   renderIcon={(isSelected) => <Ionicons name="ios-boat-outline" size={34} color={getColor(isSelected)} /> }
                 >
                   <StackNavigation id="MenuScene" initialRoute={Router.getRoute('menu')}/>
                 </TabNavigationItem>
+                {/* List Tab */}
                 <TabNavigationItem id="list"
                   renderIcon={(isSelected) => <Ionicons name="ios-list" size={34} color={getColor(isSelected)}/>}
                   selectedStyle = {styles.selectedTab} >
                   <StackNavigation id="ListScene" initialRoute = {Router.getRoute('list')}/>
                 </TabNavigationItem>
+                {/* Add / Follow Tab */}
                 <TabNavigationItem
                   id="add"
                   selectedStyle={styles.selectedTab}
                   renderIcon={(isSelected) => <Ionicons name="ios-add" size={34} color={getColor(isSelected)}/>} >
                     <StackNavigation id="AddScene" initialRoute={Router.getRoute('add')}/>
                 </TabNavigationItem>
+                {/* Chat Tab */}
                 <TabNavigationItem
                   id="chat"
                   selectedStyle={styles.selectedTab}
@@ -67,6 +71,7 @@ export default class NavigatorScene extends Component {
                   <StackNavigation id = "ChatScene"
                     initialRoute = {Router.getRoute('chat')}/>
                 </TabNavigationItem>
+                {/* Profile Tab */}
                 <TabNavigationItem
                   id="profile"
                   renderIcon={(isSelected) => <Ionicons name="ios-person-outline" size={34} color={getColor(isSelected)} /> }
@@ -86,5 +91,3 @@ const styles = StyleSheet.create({
     borderColor: '#55AADA'
   },
 });
-
-// AppRegistry.registerComponent('DQRNQUIZ', () => DQRNQUIZ);
